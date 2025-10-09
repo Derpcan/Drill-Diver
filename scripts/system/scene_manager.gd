5b9f4053bbc99334e65e0efc686dd3da3378eca7
CharlieKnipper <charlieknipper@comcast.net>
2025-10-09 18:03:01 -0400

    Fixed scene stack race condition

--- scripts/system/scene_manager.gd
+++ scripts/system/scene_manager.gd
@@ -18,24 +18,25 @@ func change_scene(scene: String) -> void:
 	
 	# Load the new scene, track it, and switch to it
 	get_tree().change_scene_to_file.call_deferred(scene)
+	await get_tree().node_added
 	scene_stack.push_back(get_tree().current_scene)
-	print("----------------------")
-	print(scene_stack)
-	print("----------------------")
 
 ## Pushes the given scene onto the scene stack, rendering all scenes
 func push_scene(scene: String) -> void:
 	if scene_stack.size() > 0:
 		# If we are rendering multiple scenes, disable all previous scenes
 		for s in scene_stack:
-			_pause_scene(s)
+			if is_instance_valid(s):
+				_pause_scene(s)
 		
 	# Load the scene and attach it to the stack root
 	var overlay_scene = load(scene).instantiate()
 	var root: Node = scene_stack.front()
 	
-	
 	scene_stack.push_back(overlay_scene)
+	print("------------")
+	print(scene_stack)
+	print("------------")
 	root.add_child(overlay_scene)
 
 ## Pops the last scene off of the stack, unrendering it
@@ -54,9 +55,7 @@ func pop_scene() -> void:
 ## Pauses a given scene (disabling input and process steps)
 func _pause_scene(scene: Node) -> void:
 	scene.process_mode = Node.PROCESS_MODE_DISABLED
-	scene.visible = false
 
 ## Unpauses a given scene (reenabling input and processing)
 func _unpause_scene(scene: Node) -> void:
 	scene.process_mode = Node.PROCESS_MODE_INHERIT
-	scene.visible = true
