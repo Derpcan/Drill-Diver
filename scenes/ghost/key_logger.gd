b024b61a141a6f4dc3dcd85a7e0195a54152c8c9
Christian <104219430+chrisgw4@users.noreply.github.com>
2025-10-11 22:14:12 -0400

    Update key_logger.gd

--- scenes/ghost/key_logger.gd
+++ scenes/ghost/key_logger.gd
@@ -10,10 +10,10 @@ var save_path:String = folder_path + "/intputlog.json"
 var elapsed_time:float = 0.0:
 	set(new_val):
 		if int(elapsed_time) != int(new_val):
-			
-			var mins:int = int((600-new_val)/60.0)
-			var secs:int = int(60*(10-mins) - new_val)
-			print_rich("[color=#34eb71]Time Left: [/color][color=#f28395]", mins, ":", secs)
+			pass
+			#var mins:int = int((600-new_val)/60.0)
+			#var secs:int = int(60*(10-mins) - new_val)
+			#print_rich("[color=#34eb71]Time Left: [/color][color=#f28395]", mins, ":", secs)
 		elapsed_time = new_val
 
 var file:FileAccess
@@ -101,8 +101,6 @@ func _log_key_input(movement_dir:Vector2, jump_direction:Vector2, is_jump_presse
 		file.store_string(JSON.stringify(JSON.from_native(keys_pressed_dict), "\t", true) + ",\n")
 	
 	
-	if elapsed_time > 600:
-		get_tree().quit()
 
 
 # When the game is being closed we end the JSON string and then close the file
