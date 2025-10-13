a20b3c5306bb55621812cd50701d46e0bdaf3074
CharlieKnipper <charlieknipper@comcast.net>
2025-10-13 18:14:52 -0400

    Refactored GameManager to store super drill meter

--- scripts/system/game_manager.gd
+++ scripts/system/game_manager.gd
@@ -12,27 +12,40 @@ extends Node
 signal start_timer
 signal stop_timer
 
-# UI Elements
-signal update_timer(seconds: float)
-
 signal fill_meter(amount: int)
 signal deplete_meter(amount: int)
 signal set_meter(amount: int)
 
-signal update_record(time: int)
+# UI Elements
+signal update_timer(seconds: float)
+signal update_record(seconds: float)
+
+signal update_meter(amount: int)
 
 @warning_ignore_restore("unused_signal")
+
+## Game Data and Stats ---------------------------------------------------------
+# Game Timer
+var _timer_running: bool = false
+var _current_time: float = 0.0 # Time elapsed in seconds
+var _best_time: float = 0.0 # The current record best time in seconds (temporary)
+
+# Super Drill
+const DRILL_METER_MIN = 0
+const DRILL_METER_MAX = 100
+var _drill_meter_charge: int = 0 # The current amount of drill meter charge
 
 func _ready():
 	print("Game Manager running...")
 	
 	self.start_timer.connect(_handle_start_timer)
 	self.stop_timer.connect(_handle_stop_timer)
+	
+	self.fill_meter.connect(_handle_fill_meter)
+	self.deplete_meter.connect(_handle_deplete_meter)
+	self.set_meter.connect(_handle_set_meter)
 
-# Game Timer Functionality -----------------------------------------------------
-var _timer_running: bool = false
-var _current_time: float = 0.0 # Time elapsed in seconds
-
+## Game Timer Functionality ----------------------------------------------------
 func _handle_start_timer() -> void:
 	_timer_running = true
 
@@ -43,6 +56,11 @@ func _handle_stop_timer() -> void:
 func set_timer_to(seconds: float) -> void:
 	_current_time = seconds
 	self.update_timer.emit(_current_time)
+
+# Sets the UI HUD record to the designated time in seconds
+func set_record_to(seconds: float) -> void:
+	_best_time = seconds
+	self.update_record.emit(_best_time)
 
 # Public accessor for the current timer value in seconds
 func get_current_time():
@@ -63,3 +81,20 @@ func _process(delta):
 	if _timer_running:
 		_current_time += delta
 		self.update_timer.emit(_current_time)
+
+## Super Drill Meter -----------------------------------------------------------
+func _handle_fill_meter(amount: int) -> void:
+	_drill_meter_charge = clamp(_drill_meter_charge + amount, DRILL_METER_MIN, DRILL_METER_MAX)
+	self.update_meter.emit(_drill_meter_charge)
+	
+func _handle_deplete_meter(amount: int) -> void:
+	_drill_meter_charge = clamp(_drill_meter_charge - amount, DRILL_METER_MIN, DRILL_METER_MAX)
+	self.update_meter.emit(_drill_meter_charge)
+	
+func _handle_set_meter(amount: int) -> void:
+	_drill_meter_charge = clamp(amount, DRILL_METER_MIN, DRILL_METER_MAX)
+	self.update_meter.emit(_drill_meter_charge)
+
+# Public accessor for the current drill meter charge
+func get_current_charge() -> int:
+	return _drill_meter_charge
