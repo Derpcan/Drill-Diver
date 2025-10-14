4f091e51ec977f27ea116211c40cfe1b604bb39f
Christian <104219430+chrisgw4@users.noreply.github.com>
2025-10-13 22:28:33 -0400

    Updated Main Menu to have a ghost play the game and a copy to clipboard for the testing data path

--- scenes/ghost/ghost.gd
+++ scenes/ghost/ghost.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 class_name Ghost
 
-@export var input_log_file_name:String = "inputlog1"
+@export var input_log_file_name:String = "inputlog1.json"
+@export var input_log_folder_path:String = "user://test_data/inputlogs"
 
 @export var movement_component:MovementComponent
 @export var jump_component:JumpComponent
