512efa2a34e6475ecfa6d37091b1e75249c9871c
Christian <chrisgw4@gmail.com>
2025-11-19 17:41:47 -0500

    Made it so some objects can be placed down in the level and be deleted

--- scenes/level editor/level_editor.gd
+++ scenes/level editor/level_editor.gd
@@ -34,16 +34,42 @@ enum tile_types {
 }
 
 
+var tiles_dictionary:Dictionary = {
+	
+}
+
+var object_dictionary:Dictionary = {
+	"Checkpoint":preload("res://scenes/checkpoint/checkpoint.tscn")
+}
+
+var tile_pos_to_object_dictionary:Dictionary[Vector2i, Object] = {
+	
+}
+
+
 
 var selected_tile:int = 0
-
+var selected_object:PackedScene = null
 
 
 func _ready() -> void:
 	# Get the tile selector
 	var tile_selector:TileSelector = level_editor_hud.get_node("TileSelector") as TileSelector
 	tile_selector.tile_selector_state_changed.connect(tile_selector_changed)
+	tile_selector.tile_selector_new_tile_selected.connect(tile_selected_changed)
 
+
+func tile_selected_changed(tile_string:String) -> void:
+	if tile_string in tiles_dictionary:
+		
+		selected_object = null
+		pass
+	
+	if tile_string in object_dictionary:
+		selected_object = object_dictionary[tile_string]
+		selected_tile = -1
+		print("Selected Object")
+	
 
 
 # Block or unblock placing tiles
@@ -63,15 +89,48 @@ func get_tile() -> Array:
 
 
 
-func set_tile( tile_map:TileMapLayer, tile_position:Vector2i,) -> void:
+func set_tile(tile_map:TileMapLayer, tile_position:Vector2i,) -> void:
+	
+	# If the tile being placed is not an object
+	if selected_object == null:
+		var tile_data:Array = get_tile()
+		var source_id:int = tile_data[0]
+		var atlas_coord:Vector2i = tile_data[1]
+		var alt_tile:int = tile_data[2]
+		
+		tile_map.set_cell(tile_position, source_id, atlas_coord, alt_tile)
+	else:
+		
+		# Check to see if a tile is there already
+		if tile_map.get_cell_tile_data(tile_position) != null:
+			tile_map.erase_cell(tile_position)
+		
+		# Check to see if the position exists in there
+		if tile_position in tile_pos_to_object_dictionary:
+			if tile_pos_to_object_dictionary[tile_position] != null:
+				tile_pos_to_object_dictionary[tile_position].queue_free()
+			tile_pos_to_object_dictionary[tile_position] = null
+		
+		var object = selected_object.instantiate()
+		object.global_position = tile_map.map_to_local(tile_position)
+		add_child(object)
+		tile_pos_to_object_dictionary[tile_position] = object
+	
+
+
+func delete_tile(tile_map:TileMapLayer, tile_position:Vector2i,):
 	var tile_data:Array = get_tile()
 	var source_id:int = tile_data[0]
 	var atlas_coord:Vector2i = tile_data[1]
 	var alt_tile:int = tile_data[2]
 	
-	tile_map.set_cell(tile_position, source_id, atlas_coord, alt_tile)
-
-
+	tile_map.set_cell(tile_position, -1, Vector2i(-1,-1), 0)
+	
+	# Check to see if the position exists in there
+	if tile_position in tile_pos_to_object_dictionary:
+		if tile_pos_to_object_dictionary[tile_position] != null:
+			tile_pos_to_object_dictionary[tile_position].queue_free()
+		tile_pos_to_object_dictionary[tile_position] = null
 
 
 func show_tile_place_preview(tile_position:Vector2) -> void:
@@ -130,5 +189,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		if is_deleting == false:
 			set_tile( physics_tilemap, tilemap_mouse_position,)
 		if is_deleting == true:
+			delete_tile(physics_tilemap, tilemap_mouse_position)
 			physics_tilemap.set_cell(tilemap_mouse_position, -1, Vector2i(-1,-1), 0)
 		
--- scenes/level editor/tile_selector.gd
+++ scenes/level editor/tile_selector.gd
@@ -11,9 +11,25 @@ class_name TileSelector
 
 signal tile_selector_state_changed(is_open:bool)
 
+signal tile_selector_new_tile_selected(new_tile_string:String)
+
 
 func _ready() -> void:
 	open_close_button.pressed.connect(open_close_button_pressed)
+	
+	for child in $ColorRect/VBoxContainer/HBoxContainer.get_children():
+		child.tile_selected.connect(_tile_selected_handler)
+
+
+func _tile_selected_handler(text:String) -> void:
+	emit_signal("tile_selector_new_tile_selected", text)
+
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("quick_toggle_tile_selector"):
+		open_close_button_pressed()
+
 
 
 func open_close_button_pressed() -> void:
