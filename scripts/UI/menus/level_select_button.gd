75b0428c3893ec7ff699fc976719725a6dd7618d
Derpcan <logangeorgi@gmail.com>
2025-11-23 17:26:42 -0500

    Revamped Title Screen

--- scripts/UI/menus/level_select_button.gd
+++ scripts/UI/menus/level_select_button.gd
@@ -1,7 +1,17 @@
-extends Button
+extends TextureButton
+var mat = material
 
 func _ready():
 	self.pressed.connect(_handle_on_press)
 
 func _handle_on_press():
 	SceneManager.change_scene("res://scenes/level_select.tscn")
+
+
+func _on_mouse_entered():
+	mat.set_shader_parameter("useHologram", true)
+	scale *= 1.1
+ 
+func _on_mouse_exited():
+	mat.set_shader_parameter("useHologram", false)
+	scale /= 1.1
--- scripts/UI/menus/option_button.gd
+++ scripts/UI/menus/option_button.gd
@@ -1,7 +1,16 @@
-extends Button
+extends TextureButton
+var mat = material
 
 func _ready():
 	self.pressed.connect(_handle_on_press)
 
 func _handle_on_press():
 	SceneManager.change_scene("res://scenes/options.tscn")
+
+func _on_mouse_entered():
+	mat.set_shader_parameter("useHologram", true)
+	scale *= 1.1
+ 
+func _on_mouse_exited():
+	mat.set_shader_parameter("useHologram", false)
+	scale /= 1.1
