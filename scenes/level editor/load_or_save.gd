66b7595a67866c1b381ee88835917ad448597860
Christian <104219430+chrisgw4@users.noreply.github.com>
2025-11-24 00:05:50 -0500

    Added buttons to level editor

--- scenes/level editor/load_or_save.gd
+++ scenes/level editor/load_or_save.gd
@@ -8,11 +8,17 @@ signal create_new_level()
 
 @export var load_button:TextureButton
 @export var create_button:TextureButton
+@export var back_button:TextureButton
 
 func _ready() -> void:
 	load_button.pressed.connect(_emit_load_level_signal)
 	create_button.pressed.connect(_emit_create_new_level_signal)
+	back_button.pressed.connect(_load_main_menu)
 	$AnimatedSprite2D/AnimationPlayer.play("loop")
+
+func _load_main_menu() -> void:
+	SceneManager.pop_scene()
+	SceneManager.change_scene("res://scenes/main_menu.tscn")
 
 
 func _emit_load_level_signal() -> void:
