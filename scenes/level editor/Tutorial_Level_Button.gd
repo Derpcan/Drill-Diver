c9ed34a81d616b820c03ce721b3d057e627f80d7
Christian <chrisgw4@gmail.com>
2025-11-29 17:53:51 -0500

    Changed tutorial level button to load scene without needing an extra popup to select file

--- scenes/level editor/Tutorial_Level_Button.gd
+++ scenes/level editor/Tutorial_Level_Button.gd
@@ -2,12 +2,7 @@ extends Button
 
 func _ready():
 	self.pressed.connect(_handle_on_press)
-	$FileDialog.file_selected.connect(file_selected)
+	#$FileDialog.file_selected.connect(file_selected)
 
 func _handle_on_press():
-	#SceneManager.change_scene("res://scenes/test_scene.tscn")
-	$FileDialog.show()
-
-
-func file_selected(file_path:String):
-	SceneManager.change_scene_extra_args("/Users/logangeorgi/Documents/GitHub/Drill-Diver/scenes/level editor/testing/tutorial_level_loader.tscn", [file_path])
+	SceneManager.change_scene("res://scenes/level editor/testing/tutorial_level_loader.tscn")
