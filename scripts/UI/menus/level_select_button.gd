dc2faaf7e6955d22e92da74fa161754da9fcd354
Derpcan <logangeorgi@gmail.com>
2025-11-30 21:16:45 -0500

    Made title screen more appealing

--- scripts/UI/menus/level_select_button.gd
+++ scripts/UI/menus/level_select_button.gd
@@ -7,7 +7,7 @@ func _ready():
 	self.pressed.connect(_handle_on_press)
 
 func _handle_on_press():
-	get_parent().get_node("ButtonSound").play()
+	get_parent().get_parent().get_node("ButtonSound").play()
 	
 	SceneManager.change_scene("res://scenes/level_select.tscn")
 
